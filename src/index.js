a91197a2aeb4464a449bf31ec9a0bcf773a97ed1
desaijay315@gmail.com <desaijay315@gmail.com>
2019-03-27 12:25:41 +0530

    latest code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Index = () => {
-  return <div>Hello React & Webpack!</div>;
+  return <div>Hello React,Webpack4,Babel7!</div>;
 };
 
 ReactDOM.render(<Index />, document.querySelector("#root"));
